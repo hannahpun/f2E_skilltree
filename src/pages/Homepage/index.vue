593a2fa382ac8b991ae59ca4b83b454c4f232cb4
hannah.lin@afusion.com <hannah.lin@afusion.com>
2018-07-31 18:54:27 +0800

    finish logic

--- src/pages/Homepage/index.vue
+++ src/pages/Homepage/index.vue
@@ -6,6 +6,7 @@ export default {
       clickList: ['basic'],
       checked: 0,
       basicData: [{
+        category: 'basic',
         id: "skills",
         icon: "category",
         title: "BASIC SKILLS",
@@ -16,18 +17,76 @@ export default {
         ],
         optional: []
       },{
+        category: 'basic',
         id: "tools",
         icon: "build",
         title: "BASIC Tools",
         recommend: [
           {title: "Git - Version Contro", checked: false},
-          {title: "BBasic Terminal Usage", checked: false}, 
+          {title: "Basic Terminal Usage", checked: false}, 
           {title: "Text Editor", checked: false},
           {title: "Heart of Reserching", checked: false},
         ],
         optional: []
+      },{
+        category: 'css',
+        id: "framework",
+        icon: "flip_to_front",
+        title: "CSS FRAMEWORK",
+        recommend: [
+          {title: "Bootstrap", checked: false}
+        ],
+        optional: [
+            {title: "UIKit", checked: false},
+            {title: "Foundation", checked: false},
+            {title: "Semantic UI", checked: false}
+        ]
+      },{
+        category: 'css',
+        id: "prepro",
+        icon: "view_quilt",
+        title: "CSS PREPROCESSORS",
+        recommend: [
+          {title: "Sass", checked: false},
+          {title: "PostCSS", checked: false}
+        ],
+        optional: [
+          {title: "Less", checked: false},
+          {title: "Stylus", checked: false}
+        ]
+      },{
+        category: '',
+        id: "",
+        icon: "",
+        title: "",
+        recommend: [
+          {title: "", checked: false},
+          {title: "", checked: false}
+        ],
+        optional: []
+      },{
+        category: '',
+        id: "",
+        icon: "",
+        title: "",
+        recommend: [
+          {title: "", checked: false},
+          {title: "", checked: false}
+        ],
+        optional: []
+      },{
+        category: '',
+        id: "",
+        icon: "",
+        title: "",
+        recommend: [
+          {title: "", checked: false},
+          {title: "", checked: false}
+        ],
+        optional: []
       }],
-      myDetail: {}
+      myDetail: {},
+      clickBtnIndex: false
     }
   },
   mounted(){
@@ -37,7 +96,14 @@ export default {
   computed: {
     isBasic(){
       return this.clickList.findIndex(item => item == "basic") != -1
+    },
+    basicLists(){
+        return this.basicData.filter(item => item.category == "basic")
+    },
+    cssLists(){
+       return this.basicData.filter(item => item.category == "css")
     }
+   
   },
   methods: {
     addItem(e){
@@ -47,12 +113,13 @@ export default {
       this.myDetail = data;
     },
     addCheck(id, index){
-      // this.showDetail(this.basicData.find(item => item.id == id))
-      this.basicData.find(item => item.id == id).recommend[index].checked = true;
-      this.myDetail = this.basicData.find(item => item.id == id);
-      console.log('hi', this.myDetail)
-      
+        this.clickBtnIndex = !this.clickBtnIndex
+      this.basicData.find(item => item.id == id).recommend[index].checked = true;      
+    },
+    checkedLength(list){
+        return list.filter(item => item.checked == true).length
     }
+    
   }
 }
 </script>
