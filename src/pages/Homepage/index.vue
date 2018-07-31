685f0fd6203358016a2b9b0306b8ae057497c52c
hannah.lin@afusion.com <hannah.lin@afusion.com>
2018-07-31 19:20:21 +0800

    update

--- src/pages/Homepage/index.vue
+++ src/pages/Homepage/index.vue
@@ -55,35 +55,116 @@ export default {
           {title: "Stylus", checked: false}
         ]
       },{
-        category: '',
-        id: "",
-        icon: "",
-        title: "",
+        category: 'css',
+        id: "architechture",
+        icon: "developer_board",
+        title: "CSS ARCHITECHTURE",
         recommend: [
-          {title: "", checked: false},
-          {title: "", checked: false}
+          {title: "OOCSS", checked: false}
+        ],
+        optional: [
+            {title: "SMACSS", checked: false},
+            {title: "BEM", checked: false}
+        ]
+      },{
+        category: 'css',
+        id: "cssSkill",
+        icon: "devices",
+        title: "CSS SKILLS",
+        recommend: [
+          {title: "Responsive", checked: false},
+          {title: "Flexbox", checked: false}
         ],
         optional: []
       },{
-        category: '',
-        id: "",
-        icon: "",
-        title: "",
+        category: 'css',
+        id: "mastery",
+        icon: "widgets",
+        title: "CSS MASTERY",
+        recommend: [],
+        optional: [
+          {title: "Grid Layout", checked: false},
+          {title: "Animation", checked: false},
+          {title: "Transform 2D, 3D", checked: false}
+        ]
+      },{
+        category: 'js',
+        id: "dom",
+        icon: "hdr_strong",
+        title: "BASIC DOM",
+        recommend: [],
+        optional: [{title: "jQuery", checked: false},]
+      },{
+        category: 'js',
+        id: "draw",
+        icon: "gradient",
+        title: "WEB DRAWING",
+        recommend: [],
+        optional: [
+          {title: "SVG", checked: false},
+          {title: "Canvas", checked: false},
+          {title: "D3.js", checked: false}
+        ]
+      },{
+        category: 'js',
+        id: "es6",
+        icon: "format_quote",
+        title: "JAVASCRIPT SKILLS",
         recommend: [
-          {title: "", checked: false},
-          {title: "", checked: false}
+          {title: "ES6", checked: false}
         ],
         optional: []
       },{
-        category: '',
-        id: "",
-        icon: "",
-        title: "",
+        category: 'js',
+        id: "jsframework",
+        icon: "developer_mode",
+        title: "JAVASCRIPT FRAMEWORK",
         recommend: [
-          {title: "", checked: false},
-          {title: "", checked: false}
+          {title: "Vue.js", checked: false},
+          {title: "Angular", checked: false},
+          {title: "React.js", checked: false}
         ],
         optional: []
+      },{
+        category: 'js',
+        id: "jsprepro",
+        icon: "nfc",
+        title: "JAVASCRIPT PREPROCESSORS",
+        recommend: [],
+        optional: [
+          {title: "TypeScript", checked: false},
+          {title: "Babel", checked: false},
+          {title: "CoffeeScript", checked: false}
+        ]
+      },{
+        category: 'maneger',
+        id: "npm",
+        icon: "device_hub",
+        title: "PACKAGE MANAGERS",
+        recommend: [
+          {title: "NPM", checked: false},
+          {title: "YARN", checked: false}
+        ],
+        optional: [{title: "Bower", checked: false}]
+      },{
+        category: 'maneger',
+        id: "task",
+        icon: "import_contacts",
+        title: "TASK RUNNERS",
+        recommend: [
+          {title: "npm scripts", checked: false},
+          {title: "gulp", checked: false}
+        ],
+        optional: [{title: "grunt", checked: false}]
+      },{
+        category: 'maneger',
+        id: "buildtools",
+        icon: "table_chart",
+        title: "BUILD TOOLS",
+        recommend: [
+          {title: "Webpack", checked: false}
+        ],
+        optional: [{title: "Parcel", checked: false}]
       }],
       myDetail: {},
       clickBtnIndex: false
@@ -102,10 +183,19 @@ export default {
     },
     cssLists(){
        return this.basicData.filter(item => item.category == "css")
+    },
+    jsLists(){
+       return this.basicData.filter(item => item.category == "js")
+    },
+    managerLists(){
+       return this.basicData.filter(item => item.category == "maneger")
     }
    
   },
   methods: {
+    isCategory(category){
+        return this.clickList.findIndex(item => item == category) != -1
+    },
     addItem(e){
       this.clickList.push(e);
     },
